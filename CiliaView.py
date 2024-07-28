333c9e76cd6cbb3adac1a77efe04afe74387e033
NEbrahimi <eb.nazanin@gmail.com>
2024-07-28 14:17:03 +1000

    changed path for inputs

--- CiliaView.py
+++ CiliaView.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json
 from PIL import Image
 
-input_path = r"/home/z3541106/ondemand/CiliaView/Input_Files"
+input_path = "$HOME/CiliaView/Input_Files/"
 
 # Load data
 with open(input_path + 'genes.json') as f:
